98e4e6bd87117ce54b24cf92dea9f986a1394823
Giang Tran Truong <ttgiang@tma.com.vn>
2020-05-22 16:23:44 +0700

    JDBC Authencation

--- src/main/java/com/river/config/EmployeeSecurityConfig.java
+++ src/main/java/com/river/config/EmployeeSecurityConfig.java
@@ -1,16 +1,35 @@
 package com.river.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class EmployeeSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+	
+	@Autowired
+    DataSource dataSource;
+
+	//Enable jdbc authentication
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder()); ;
+    }
 	
 	@Override
     public void configure(WebSecurity web) throws Exception {
@@ -33,12 +52,12 @@ public class EmployeeSecurityConfig extends WebSecurityConfigurerAdapter{
         http.csrf().disable();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder authenticationMgr) throws Exception {
-        authenticationMgr.inMemoryAuthentication()
-        	.withUser("user").password("{noop}user").authorities("ROLE_USER")
-        	.and()
-        	.withUser("admin").password("{noop}admin").authorities("ROLE_USER", "ROLE_ADMIN");
-    }
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder authenticationMgr) throws Exception {
+//        authenticationMgr.inMemoryAuthentication()
+//        	.withUser("user").password("{noop}user").authorities("ROLE_USER")
+//        	.and()
+//        	.withUser("admin").password("{noop}admin").authorities("ROLE_USER", "ROLE_ADMIN");
+//    }
 
 }
